7ccdeccbfa5f205c7ef3e5ae7655ca01d2669bfa
pawan kumar <hcl.pawan@gmail.com>
2025-12-27 12:32:25 +0530

    added terraform code and updated workfloww with oidc

---
+++ main.tf
@@ -0,0 +1,30 @@
+//write the code or security group here
+    
+    variable "region" {
+        description = "The AWS region to deploy resources in"
+        type        = string
+        default     = "us-east-1"
+      
+    }
+    provider "aws" {
+        region = var.region
+    }
+
+    resource "aws_security_group" "example" {
+        name        = "example"
+        description = "Example security group"
+
+        ingress {
+            from_port   = 80
+            to_port     = 80
+            protocol    = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        egress {
+            from_port   = 0
+            to_port     = 0
+            protocol    = "-1"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+    }
